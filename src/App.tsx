f0c5848023b794d025b90b10da57e5660325c609
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-26 00:41:46 -0400

    feat: initil configuration vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <p>Hello World</p>
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
