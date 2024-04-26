7d40a0f6c3a8e184d150e3400202b1cc0aa74b13
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-26 16:09:48 -0400

    style: spand heigth to span count

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,7 +1,7 @@
 export const Counter = () => {
   return (
     <>
-      <span className="inline-block bg-primary-blue text-white w-8 h-[25px] text-center rounded-md">
+      <span className="inline-block bg-primary-blue text-white w-8 h-[30px] text-center rounded-md">
         0
       </span>
     </>
