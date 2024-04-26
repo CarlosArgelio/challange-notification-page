6ac3bbe4d43625ef552ea90f0cfcfac1041b762b
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-26 14:55:57 -0400

    style: mockup mobile notification

--- src/Components/NotificationCard.tsx
+++ src/Components/NotificationCard.tsx
@@ -1,68 +1,132 @@
-import { avatars } from "./../assets/images";
+import { avatars, ImageGhess } from "./../assets/images";
 
 export const NotificationCard = () => {
   return (
     <>
       {/* Notifications */}
       <div>
-        <div>
-          <img src={avatars.markWebber} alt="" />
-          <p>
-            {" "}
-            <span>Mark Webber</span> <span>reacted to your recent post</span>{" "}
-            <span>My first tournament today!</span>{" "}
-            <span className="status">*</span>
-          </p>
-          <p>1m ago</p>
+        <div className="flex gap-3 p-4 text-sm bg-very-ligth-graying-blue text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.markWebber} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Mark Webber
+              </span>{" "}
+              <span>reacted to your recent post</span>{" "}
+              <span className="font-semibold hover:text-primary-blue">
+                My first tournament today!
+              </span>{" "}
+              {/* status */}
+              <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span>
+            </p>
+            <p className="text-grayish-blue font-medium">1m ago</p>
+          </div>
         </div>
 
-        <div>
-          <p>
-            {" "}
-            <span>Angela Gray</span> <span>followed you</span>
-          </p>
-          <p>5m ago</p>
+        <div className="flex gap-3 p-4 text-sm bg-very-ligth-graying-blue text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.angelaGray} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Angela Gray
+              </span>{" "}
+              <span>followed you</span> {/* status */}
+              <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span>
+            </p>
+            <p className="text-grayish-blue font-medium">5m ago</p>
+          </div>
         </div>
 
-        <div>
-          <p>
-            {" "}
-            <span>Rizky Hasanuddin</span>{" "}
-            <span>sent you a private message</span>
-          </p>
-          <p>5 days ago</p>
-          <p>
-            Hello, thanks for setting up the Chess Club. I've been a member for
-            a few weeks now and I'm already having lots of fun and improving my
-            game.
-          </p>
+        <div className="flex gap-3 p-4 text-sm bg-very-ligth-graying-blue text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.jacobThompson} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Jacob Thompon
+              </span>{" "}
+              <span>has joined your group</span>{" "}
+              <span className="font-bold text-primary-blue">Chess Club</span>{" "}
+              {/* status */}
+              <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span>
+            </p>
+            <p className="text-grayish-blue font-medium">1d ago</p>
+          </div>
         </div>
 
-        <div>
-          <p>
-            {" "}
-            <span>Kimberly Smith</span> <span>commented on your picture</span>
-          </p>
-          <p>1 week ago</p>
+        <div className="flex gap-3 p-4 text-sm text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.rizkyHasanuddin} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Rizky Hasanuddin
+              </span>{" "}
+              <span>sent you a private message</span> {/* status */}
+              {/* <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span> */}
+            </p>
+            <p className="text-grayish-blue font-medium mb-2">5 days ago</p>
+            <p className="border border-ligth-grayish-blue-2 p-1 rounded-lg hover:bg-ligth-grayish-blue-1">
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
+            </p>
+          </div>
         </div>
 
-        <div>
-          <p>
-            {" "}
-            <span>Nathan Peterson</span>{" "}
-            <span>reacted to your recent post</span>{" "}
-            <span>5 end-game strategies to increase your win rate</span>
-          </p>
-          <p>2 weeks ago</p>
+        <div className="flex gap-3 p-4 text-sm text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.kimberlySmith} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Kimberly Smith
+              </span>{" "}
+              <span>commented on your picture</span> {/* status */}
+              {/* <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span> */}
+            </p>
+            <p className="text-grayish-blue font-medium mb-2">1 week ago</p>
+          </div>
+          <img src={ImageGhess} alt="" className="w-10 h-10" />
         </div>
 
-        <div>
-          <p>
-            {" "}
-            <span>Anna Kim</span> <span>left the group</span>{" "}
-            <span>Chess Club</span>
-          </p>
-          <p>2 weeks ago</p>
+        <div className="flex gap-3 p-4 text-sm text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.natashaPeterson} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Nathan Peterson
+              </span>{" "}
+              <span>reacted to your recent post</span>{" "}
+              <span className="font-semibold hover:text-primary-blue">
+                5 end-game strategies to increase your win rate
+              </span>{" "}
+              {/* status */}
+              {/* <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span> */}
+            </p>
+            <p className="text-grayish-blue font-medium">2 weeks ago</p>
+          </div>
+        </div>
+
+        {/* bg-very-ligth-graying-blue */}
+        <div className="flex gap-3 p-4 text-sm  text-very-dark-blue cursor-pointer mb-1 rounded-lg">
+          <img src={avatars.annaKim} alt="" className="w-10 h-10" />
+          <div>
+            <p>
+              {" "}
+              <span className="font-bold text-very-dark-blue hover:text-primary-blue">
+                Anna Kim
+              </span>{" "}
+              <span>left the group</span>{" "}
+              <span className="font-bold text-primary-blue">Chess Club</span>{" "}
+              {/* status */}
+              {/* <span className="inline-block w-2 h-2 bg-primary-red rounded-full ml-2"></span> */}
+            </p>
+            <p className="text-grayish-blue font-medium">2 weeks ago</p>
+          </div>
         </div>
       </div>
     </>
